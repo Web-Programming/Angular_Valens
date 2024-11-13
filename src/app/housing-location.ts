c5479e27f1d2cbeac9e5071b56e43b2e7083e03d
ValensFlorencia <160854619+ValensFlorencia@users.noreply.github.com>
2024-11-13 20:29:06 +0700

    angular

---
+++ src/app/housing-location.ts
@@ -0,0 +1,11 @@
+export interface HousingLocation {
+    id : 0,
+    name : "Citra Grand City",
+    city : "Palembang",
+    state : "ID",
+    photo : "https://id.images.search.yahoo.com/images/view;_ylt=AwrPrjF4pjRn3bEfVr_NQwx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2QzZGIzYTBkZDk1MDQxM2Q5MjIzYmQ5ZTI1MzgzMGQ5BGdwb3MDMgRpdANiaW5n?back=https%3A%2F%2Fid.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dcitra%2Bgrand%2Bcity%26type%3DE211ID885G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D2&w=1920&h=1361&imgurl=citragrandcity.co.id%2Fwp-content%2Fuploads%2F2021%2F05%2FCalathea.jpg&rurl=https%3A%2F%2Fcitragrandcity.co.id%2Fproject%2Fcalathea%2F&size=319KB&p=citra+grand+city&oid=d3db3a0dd950413d9223bd9e253830d9&fr2=piv-web&fr=mcafee&tt=Calathea+-+CitraGrand+City&b=0&ni=21&no=2&ts=&tab=organic&sigr=zD6DPnveB5T_&sigb=BIuzhKCWEiod&sigi=5mbjXAzH5LIp&sigt=VrliZqD2px.L&.crumb=vwHk4kwQGpV&fr=mcafee&fr2=piv-web&type=E211ID885G0",
+    availableUnits : 12,
+    wifi : true,
+    laundry :true,
+
+}
